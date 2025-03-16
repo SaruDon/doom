2240ffe5c5f8f4f3c516513a3f1f2595edde1f59
Sarvesh Khamkar <sarveshkhamkar321@gmail.com>
2025-03-16 19:06:16 +0530

    Meeting setup page

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -7,21 +7,28 @@ import { useUser } from "@clerk/nextjs";
 import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
 import React, { useState } from "react";
 
-const Meeting = ({ params: { id } }: { params: { id: string } }) => {
+const Meeting = ({ params }: { params: { id: string | Promise<string> } }) => {
+  // Unwrap the id if it's a Promise
+  const id = params.id instanceof Promise ? React.use(params.id) : params.id;
+
   const { user, isLoaded } = useUser();
   const [isSetupComplete, setIsSetupComplete] = useState(false);
 
   const { call, isCallLoading } = useGetCallById(id);
 
-  if (isCallLoading || isLoaded) {
-    <Loader />;
+  if (isCallLoading || !isLoaded) {
+    return <Loader />;
   }
 
   return (
     <div className="h-screen w-full">
       <StreamCall call={call}>
         <StreamTheme>
-          {!isSetupComplete ? <MeetingSetup /> : <MeetingRoom />}
+          {!isSetupComplete ? (
+            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+          ) : (
+            <MeetingRoom />
+          )}
         </StreamTheme>
       </StreamCall>
     </div>
